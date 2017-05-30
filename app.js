9618007a6c25ed6d98bad92616275959c5cdba0c
RayAdams <cramerrachael@gmail.com>
2017-05-30 14:44:35 -0400

    Added Files

---
+++ app.js
@@ -0,0 +1,18 @@
+//Console.log the following:
+
+// * A list of your interests
+// * A list of your past positions that includes company/place name, title, short description of what you did
+// * A list of your skills
+//* In your skill list, skills that are "cool" should begin with `BAM:`
+
+var name = "rachael adams".toUpperCase();
+var job = "Full Stack Student";
+var bio = "I love donuts";
+var interests = ["*Cooking", "*Reading", "*"];
+
+console.log("Name: " + name);
+console.log("Career: " + job);
+console.log("Description: " + bio);
+<br>
+<br>
+console.log(interests);
